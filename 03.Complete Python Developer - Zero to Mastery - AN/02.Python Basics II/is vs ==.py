c5a81cdb9f54db434b294ea1086f4ea49226f025
Phone Thiri Yadana <ptyadana@gmail.com>
2020-03-29 20:09:31 +0800

    logical operators and is vs ==

---
+++ 03.Complete Python Developer - Zero to Mastery - AN/02.Python Basics II/is vs ==.py
@@ -0,0 +1,16 @@
+print(True == 1)
+print('' == 1)
+print([] == 1) 
+print(10 == 10.0)
+print([] == [])
+print([1,2,3] == [1,2,3])
+
+print("-----------")
+
+print(True is 1)
+print(True is True)
+print('' is 1)
+print([] is 1) 
+print(10 is 10.0)
+print([] is[])
+print([1,2,3] is [1,2,3]) #different memory space

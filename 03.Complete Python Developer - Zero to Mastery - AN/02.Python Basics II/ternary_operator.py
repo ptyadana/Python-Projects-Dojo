c2db32a8af59ea04af2d492ab88c16331e12f160
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-29 19:44:53 +0800

    add truthy_falsely and ternary operators

---
+++ 03.Complete Python Developer - Zero to Mastery - AN/02.Python Basics II/ternary_operator.py
@@ -0,0 +1,10 @@
+#Ternary Operator
+
+# condition_if_true if condition else condition_if_false
+is_friend = True
+can_message = "can message" if is_friend else "not allowed"
+print(can_message)
+
+is_friend2 = False
+can_message2 = "can message" if is_friend2 else "not allowed"
+print(can_message2)
---
+++ 03.Complete Python Developer - Zero to Mastery - AN/02.Python Basics II/truth_falsey.py
@@ -0,0 +1,6 @@
+username = "john"
+password = "123"
+
+# if username and password both exists
+if username and password:
+    print("you have both username and password")

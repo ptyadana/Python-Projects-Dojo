4a81517e4d54d1895786e992afdec37fc709a8d7
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-29 20:09:31 +0800

    logical operators and is vs ==

---
+++ 03.Complete Python Developer - Zero to Mastery - AN/02.Python Basics II/8. Exercise Logical Operators.py
@@ -0,0 +1,9 @@
+is_magician = False
+is_expert = False
+
+if is_magician and is_expert:
+    print("You are a master magician !")
+elif is_magician and not is_expert:
+    print("at least you are getting there.")
+else:
+    print("you need magic powers.")

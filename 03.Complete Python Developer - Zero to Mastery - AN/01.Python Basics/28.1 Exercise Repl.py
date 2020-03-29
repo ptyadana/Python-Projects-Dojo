c66a69de6132be02cea7ce303011d96ae5fb0030
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-29 13:25:21 +0800

    add string, matrix, lists

---
+++ 03.Complete Python Developer - Zero to Mastery - AN/01.Python Basics/28.1 Exercise Repl.py
@@ -0,0 +1,5 @@
+# using this list: 
+basket = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+# access "Oranges" and print it:
+
+print(basket[1][1][0])
---
+++ 03.Complete Python Developer - Zero to Mastery - AN/01.Python Basics/lists.py
@@ -0,0 +1,35 @@
+list1 = ["a","b","c"]
+list2 = [1,2,3]
+list3 = ["A",2,"CDCC",10,True,"BBB",False]
+
+amazon_cart = [
+    "pens",
+    "notbooks",
+    "shampoo",
+    "toothbrush"
+]
+
+print(amazon_cart)
+print(amazon_cart[0::2])
+
+amazon_cart [1] = "laptop"
+
+print(amazon_cart)
+print(amazon_cart[0:3])
+
+print("-----------------------------")
+
+print(amazon_cart)
+new_cart = amazon_cart
+new_cart[0] = "icecream"
+
+print("--------After pointing newcart to amazon cart---------------------")
+print(amazon_cart)
+print(new_cart)
+
+#to avoid original list item gets changed, we need to use the copy of original list
+print("--------After copying amzon cart to newcart ---------------------")
+new_cart = amazon_cart[:]
+new_cart[0] = "Vitamin"
+print(amazon_cart)
+print(new_cart)

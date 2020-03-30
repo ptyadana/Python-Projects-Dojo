4bbc5fbf0128935c11436a4251f9a53996d90346
Phone Thiri Yadana <ptyadana@gmail.com>
2020-03-30 19:56:35 +0800

    add scope global nonlocal

---
+++ 03.Complete Python Developer - Zero to Mastery - AN/02.Python Basics II/34.1 Exercise Repl.py
@@ -0,0 +1,15 @@
+# Scope - what variables do I have access to?
+def outer():
+    x = "local"
+    def inner():
+        nonlocal x
+        x = "nonlocal"
+        print("inner:", x)
+    inner()
+    print("outer:", x)
+outer()
+
+#1 - start with local
+#2 - Parent local?
+#3 - global
+#4 - built in python functions

36923c55e437d02123d87aadf76519a24b86a4f5
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-31 21:08:19 +0800

    list, set, dictionary comprehension

---
+++ 03.Complete Python Developer - Zero to Mastery - AN/04.Advanced Python Functional Programming/set_comprehension.py
@@ -0,0 +1,14 @@
+my_list = {char for char in 'hello'}
+print(my_list)
+
+#0 - 99
+my_list2 = {num for num in range(0,100)}
+print(my_list2)
+
+#square of 0-99
+my_list3 = {num**2 for num in range(0,100)}
+print(my_list3)
+
+#only even number of (square of 0-99)
+my_list4 = {num**2 for num in range(0,100) if num%2 == 0}
+print(my_list4)

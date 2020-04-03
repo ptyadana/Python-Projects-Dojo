f905dc9de946db1377f6b0994533dc5f52049ad7
Phone Thiri Yadana <ptyadana@gmail.com>
2020-04-03 22:28:20 +0800

    modules and packages

---
+++ 03.Complete Python Developer - Zero to Mastery - AN/08.Modules In Python/module_utilities.py
@@ -0,0 +1,5 @@
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1/num2
---
+++ 03.Complete Python Developer - Zero to Mastery - AN/08.Modules In Python/shopping/shopping_cart.py
@@ -0,0 +1,4 @@
+def buy(item):
+    cart = []
+    cart.append(item)
+    return cart

f603c2342a07408926eab40de809489637f5f39f
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-04-03 22:28:20 +0800

    modules and packages

---
+++ 03.Complete Python Developer - Zero to Mastery - AN/08.Modules In Python/module_example.py
@@ -0,0 +1,11 @@
+import module_utilities
+# from shopping import shopping_cart
+from shopping.shopping_cart import buy
+
+print(module_utilities.multiply(10,2))
+print(module_utilities.divide(100,2))
+
+# print(shopping_cart.buy("apple"))
+
+#cleaner version
+print(buy("apple"))

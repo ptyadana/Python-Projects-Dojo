4e64881dfae54497f201ce56504562d535ca5c58
Phone Thiri Yadana <ptyadana@gmail.com>
2020-04-08 17:39:44 +0800

    modules and builtin modules

---
+++ 03.Complete Python Developer - Zero to Mastery - AN/08.Modules In Python/builtin_modules_2.py
@@ -0,0 +1,9 @@
+import sys
+
+first = sys.argv[1]
+second = sys.argv[2]
+print(f'hello {first} , {second}')
+
+#testing
+#run from terminal
+#python builtin_modules_2.py richard drahcir
--- 03.Complete Python Developer - Zero to Mastery - AN/08.Modules In Python/module_utilities.py
+++ 03.Complete Python Developer - Zero to Mastery - AN/08.Modules In Python/module_utilities.py
@@ -1,3 +1,5 @@
+print(__name__)
+
 def multiply(num1, num2):
     return num1 * num2
 

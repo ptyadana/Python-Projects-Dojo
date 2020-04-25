076ae2903458f6dd45ae533526135f0a08f3b0a9
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-04-25 19:12:44 +0800

    update readme

--- 05.Managing Python Packages and Virtual Environments - Pluralsight/m2_pip/get_rates.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-response = requests.get(
-    "https://api.exchangeratesapi.io/latest?symbols=USD")
-print(response.text)
--- 05.Managing Python Packages and Virtual Environments - Pluralsight/m3_venv/get_rates.py
+++
@@ -1,7 +0,0 @@
-import requests
-from box import Box
-
-response = requests.get(
-    "https://api.exchangeratesapi.io/latest?symbols=USD")
-b=Box(response.json())
-print("1 Euro is ", b.rates.USD, " dollars.")

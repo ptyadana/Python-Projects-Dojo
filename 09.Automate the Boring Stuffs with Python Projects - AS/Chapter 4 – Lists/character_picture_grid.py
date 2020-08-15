8c746c6d310f970773aaa10e4025119c74eacddf
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-08-15 11:42:56 +0800

    character picture grid completed

---
+++ 09.Automate the Boring Stuffs with Python Projects - AS/Chapter 4 – Lists/character_picture_grid.py
@@ -0,0 +1,26 @@
+""""
+print the picture of the lists in terms of grid as below shape
+
+..OO.OO..
+.OOOOOOO.
+.OOOOOOO.
+..OOOOO..
+...OOO...
+....O....
+
+""""
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for y in range(len(grid[0])):
+    for x in range(len(grid)):
+        print(grid[x][y], end="")
+    print("")

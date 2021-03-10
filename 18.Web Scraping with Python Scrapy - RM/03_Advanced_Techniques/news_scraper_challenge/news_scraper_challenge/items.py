dd40f6ea44deddc8b686a101e9140cb108d56e8d
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2021-03-10 15:24:03 +0800

    collecting url from sitemap

---
+++ 18.Web Scraping with Python Scrapy - RM/03_Advanced_Techniques/news_scraper_challenge/news_scraper_challenge/items.py
@@ -0,0 +1,16 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class NewsArticle(scrapy.Item):
+    url = scrapy.Field()
+    source = scrapy.Field()
+    title = scrapy.Field()
+    description = scrapy.Field()
+    date = scrapy.Field()
+    author = scrapy.Field()
+    text = scrapy.Field()
---
+++ 18.Web Scraping with Python Scrapy - RM/03_Advanced_Techniques/news_scraper_challenge/news_scraper_challenge/pipelines.py
@@ -0,0 +1,14 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+
+class NewsScraperChallengePipeline:
+    def process_item(self, item, spider):
+        item.author = item.author.replace(', CNN', '')
+        return item
